8265d0be76bf75a8765cad5a76bc3fbaaf05d0c8
Ayan <imluckyayan@gmail.com>
2024-12-11 10:59:32 +0530

    Dec 11 10:59

--- Dockerfile
+++ Dockerfile
@@ -3,11 +3,9 @@ FROM debian:latest
 # Copy script to the container
 COPY install.sh /install.sh
 
-# Make the script executable
 RUN chmod +x /install.sh && \
-    apt-get update && \
-    apt-get install -y git curl vim wget redis-server systemctl && \
-    apt-get clean
+    ./install.sh
 
-# Start with bash and execute the script
-CMD ["/bin/bash", "-c", "/install.sh && tail -f /dev/null"]
+RUN service redis-server start
+
+CMD ["/bin/bash", "-c", " echo \"DOCKER SETUP COMPLETED\" && tail -f /dev/null"]
--- install.sh
+++ install.sh
@@ -1,18 +1,25 @@
 #!bin/bash
 
+# Updating & Installing Dependencies
+apt-get update
+apt-get install -y git curl vim wget redis-server systemctl
+apt-get clean
+
+# Installing NVM
 if ! command -v nvm &> /dev/null
 then
     echo 'Installing nvm...'
     curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash
     echo 'nvm Installed'
 else
-    echo 'nvm is already installed'
+    echo 'nvm is alrea:dy installed'
 fi
 
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
+# Installing Node.js
 if ! command -v node &> /dev/null
 then
     echo 'Installing Node.js...'
@@ -23,8 +30,8 @@ else
     echo 'Node.js is already installed'
 fi
 
+# Enabling typescript globally
 npm install -g typescript
-
 
 if [ ! -d "data/talawa-api" ]; then
     cd data
@@ -34,7 +41,5 @@ if [ ! -d "data/talawa-api" ]; then
 else
     cd data/talawa-api
 fi
-
-service redis-server start # port 6379  
 
 echo "DOCKER SETUP COMPLETED !!!"
